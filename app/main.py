c4c9c51b3ef566d8c811ad9ce4e547c6291c2481
Bernardo Andrade <berdfandrade@gmail.com>
2026-02-11 22:13:30 -0300

    --

--- app/main.py
+++ app/main.py
@@ -1,16 +1,20 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
-from app.core.database import connect_to_mongo, close_mongo_connection
 from app.api import router
+from app.core.database import connect_to_mongo, close_mongo_connection
+from contextlib import asynccontextmanager
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    # startup
     await connect_to_mongo()
     yield
     await close_mongo_connection()
 
 
-app = FastAPI(title="My FastAPI App", description="My FastAPI App", lifespan=lifespan)
+app = FastAPI(
+    title="Contract lens", description="Api Contract Lens 1.0", lifespan=lifespan
+)
+
 
 app.include_router(router)
