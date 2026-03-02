dde7afa2608880383f4eeedbe31d18dfca4a8894
Bernardo Andrade <berdfandrade@gmail.com>
2026-03-01 23:43:46 -0300

    frontend : add clsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import "./App.css";
+import LandingPage from "./pages/Landing";
 
 function App() {
-  return <h1 className="text-3xl font-bold"> Hello world! </h1>;
+  return <LandingPage />;
 }
 
 export default App;
---
+++ frontend/src/pages/Landing/index.tsx
@@ -0,0 +1,18 @@
+export default function LandingPage() {
+  return (
+    <div className="flex flex-col gap-10 min-h-full">
+      <div className="border">
+        <span className="font-bold">Entenda seu contrato de aluguel</span>
+        <p>em minutos</p>
+      </div>
+      <div className="border">
+        <span className="font-bold">Entenda seu contrato de aluguel</span>
+        <p>em minutos</p>
+      </div>
+      <div className="border">
+        <span className="font-bold">Entenda seu contrato de aluguel</span>
+        <p>em minutos</p>
+      </div>
+    </div>
+  );
+}
